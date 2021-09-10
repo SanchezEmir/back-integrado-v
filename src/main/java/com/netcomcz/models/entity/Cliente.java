25f713b6a17a17befcba7f6fba3bac423956cb9f
@cookie <emir2104@gmail.com>
2021-09-10 04:38:12 -0500

    validaciones de fecha

--- src/main/java/com/netcomcz/models/entity/Cliente.java
+++ src/main/java/com/netcomcz/models/entity/Cliente.java
@@ -8,12 +8,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
@@ -38,16 +38,19 @@ public class Cliente implements Serializable{
 	@Email(message = "el correo no es valido")
 	@Column(nullable = false, unique = true)
 	private String email;
-
+	
+	@NotNull(message = "no puede estar vacio")
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
 	private Date createAt;
 	
+	/*
+	 * crea de manera acutomatica la fecha
 	@PrePersist
 	public void prePersist() {
 		createAt = new Date();
 	}
-
+	*/
 	public Long getId() {
 		return id;
 	}
